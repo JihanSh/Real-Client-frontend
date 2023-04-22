279cc35f9d01fa83e6a00086d90677cea1def733
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2023-04-22 15:25:39 +0300

    added client information

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -15,20 +15,23 @@ const About = () => {
     <>
     <Box bgcolor="#D9D9D9" p={{ xs: 2, sm: 3, md: 5 }} mt={{ xs: 3, sm: 4, md: 5 }}  width={{ xs: '100%', sm: '80%', md: '65%' }} ml={{ xs: -1, sm: 4, md: 35 }} >
         <Typography variant="h4"  ml={{ xs: 2, sm: 3, md: 5 }} gutterBottom>
-        About Us
+          About Us
         </Typography>
     </Box>
 
-    <Box p={{ xs: 3, sm: 4, md: 12 }} mb={-10} ml={{ xs: 2, sm: 3, md: 32 }} mt={{ xs: -3, sm: -4, md: -6 }} >
+    <Box p={{ xs: 3, sm: 4, md: 12 }} mb={-10} ml={{ xs: 2, sm: 3, md: 32 }} mt={{ xs: -3, sm: -4, md: -10 }} >
         <Typography variant="h5" mt={{ xs: 3, sm: 4 }} gutterBottom>
             FEEL FREE TO CONTACT US
         </Typography>
         <Typography variant="body1" align="justify" mt={3} gutterBottom>
-        Torquent mattis iste nisl rerum, placerat do wisi. Interdum diamlorem sociis, mollitia eaque perspiciatis, esee nascetur morbi laboriosam sociis at nunc pede felis.
+          The time for the delivery: 2 to 7 days
+        </Typography>
+        <Typography variant="body1" align="justify" mt={3} gutterBottom>
+          How much does the delivery cost? 2.5$
         </Typography>
       </Box>
 
-    <Box mt={5} ml={{ xs: 2, sm: 4, md: 32 }} p={{ xs: 2, sm: 4, md: 6 }}>
+    <Box mt={{ xs: 6, sm: 4, md: -1 }} ml={{ xs: 2, sm: 4, md: 32 }} p={{ xs: 2, sm: 4, md: 6 }}>
       <Grid container spacing={{ xs: 2, sm: 4, md: 6 }}>
         <Grid item xs={12} md={6}>
           <Box  p={{ xs: 2, sm: 4 }} display="flex" alignItems="center">
@@ -38,7 +41,7 @@ const About = () => {
                 Location
               </Typography>
               <Typography variant="body1">
-                123 Main St, Anytown USA
+                Pick up available from Saida
               </Typography>
             </Box>
           </Box>
@@ -52,7 +55,7 @@ const About = () => {
                 Email
               </Typography>
               <Typography variant="body1">
-                info@example.com
+                zone.outlet.00@gmail.com
               </Typography>
             </Box>
           </Box>
@@ -66,7 +69,7 @@ const About = () => {
                 Phone
               </Typography>
               <Typography variant="body1">
-                123-456-7890
+                +961 71958446
               </Typography>
             </Box>
           </Box>
@@ -75,8 +78,8 @@ const About = () => {
         <Grid item xs={12} md={6} >
           <Box  p={{ xs: 2, sm: 4 }}  display="flex" alignItems="center" >
             <Box display="flex" alignItems="center" >
-             <a  href="/"> <FacebookIcon color="primary" fontSize="large" style={iconStyle} /></a>
-             <a href="/"> <InstagramIcon color="primary" fontSize="large" style={iconStyle} /> </a>
+             <a  href="https://www.facebook.com/ZONE.Outlet.0?mibextid=LQQJ4d"> <FacebookIcon color="primary" fontSize="large" style={iconStyle} /></a>
+             <a href="https://instagram.com/_zone_outlet_?igshid=YmMyMTA2M2Y="> <InstagramIcon color="primary" fontSize="large" style={iconStyle} /> </a>
             </Box>
             <Box ml={{ xs: 2, sm: 4 }} >
               <Typography variant={variant} gutterBottom>
